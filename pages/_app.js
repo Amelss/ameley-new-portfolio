40939111ea8015285aa95af4537da4d6c1d52eba
Amelss <ameleyka@gmail.com>
2024-04-04 18:49:11 +0100

    Adding fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,45 @@
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
+import localFont from "next/font/local";
+
+const montserrat = localFont({
+  src: [
+    {
+      path: "../public/fonts/Montserrat-Bold.ttf",
+      weight: "700",
+    },
+    {
+      path: "../public/fonts/Montserrat-Black.ttf",
+      weight: "900",
+    },
+    {
+      path: "../public/fonts/Montserrat-ExtraBold.ttf",
+      weight: "700",
+    },
+    {
+      path: "../public/fonts/Montserrat-Medium.ttf",
+      weight: "700",
+    },
+    {
+      path: "../public/fonts/Montserrat-Regular.ttf",
+      weight: "700",
+    },
+    {
+      path: "../public/fonts/Montserrat-Light.ttf",
+      weight: "700",
+    },
+    {
+      path: "../public/fonts/Montserrat-ExtraLight.ttf",
+      weight: "700",
+    },
+  ],
+
+  variable: "--font-montserrat",
+});
 
 export default function App({ Component, pageProps }) {
   return (
-    <main>
+    <main className={`${montserrat.variable} font-sans`}>
       <Layout>
         
         <Component {...pageProps} />
